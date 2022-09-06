1ea3cd67513627f921e37a65d629204dac32a729
Beren Kuday <kuday.site@gmaik.com>
2022-09-06 16:04:00 +0300

    Main code has been added.

--- Sources/KeyChainProtection2/KeyChainProtection2.swift
+++ Sources/KeyChainProtection2/KeyChainProtection2.swift
@@ -1,6 +1,44 @@
+import UIKit
+
 public struct KeyChainProtection2 {
-    public private(set) var text = "Hello, World!"
+
 
     public init() {
+        
     }
+    
+    func start(){
+        if(firsTimeLunc()){
+            print("First")
+            self.deleteAllKeyChains()
+        }
+        else
+        {
+            print("Not first")
+        }
+    }
+    
+    private func firsTimeLunc() -> Bool{
+        let defaults = UserDefaults.standard
+        if (defaults.bool(forKey: "firstTimeCheck4keyChain")){
+            return false
+        }
+        else{
+            defaults.set(true, forKey: "firstTimeCheck4keyChain")
+            return true
+        }
+    }
+    
+    private func deleteAllKeyChains(){
+        let secItemClasses = [kSecClassGenericPassword, kSecClassInternetPassword, kSecClassCertificate, kSecClassKey, kSecClassIdentity]
+        for itemClass in secItemClasses {
+            let spec: NSDictionary = [kSecClass: itemClass]
+            SecItemDelete(spec)
+            
+        }
+        print("All data has been deleted.")
+        
+    }
+    
 }
+
